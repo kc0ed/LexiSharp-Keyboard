6084610180b40fd60e46e7c4370af1816be5f76d
Luolin Zhang <zhang1577006721@qq.com>
2025-10-12 19:00:13 +0800

    feat(guide): 增强快速指南

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brycewg.asrkb"
         minSdk =31
         targetSdk = 34
-        versionCode = 38
-        versionName = "2.6.4"
+        versionCode = 39
+        versionName = "2.6.5"
     }
 
     signingConfigs {
