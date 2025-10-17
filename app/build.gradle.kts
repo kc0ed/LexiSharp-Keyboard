1bce06ce3fbb5d6200cf0f16662d9e9e18cc995c
BryceWG <zhang1577006721@qq.com>
2025-10-17 21:17:14 +0800

    chore(release): 更新版本号至 3.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brycewg.asrkb"
         minSdk =29
         targetSdk = 34
-        versionCode = 65
-        versionName = "3.0.2"
+        versionCode = 66
+        versionName = "3.0.3"
 
         // 仅打包 arm64-v8a 以控制体积；可按需扩展
         ndk {
